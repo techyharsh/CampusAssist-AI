9bf2097bdb3bc747a33ba7e08261db376867501a
Harsh Pratap Singh <pratapharsh2005@gmail.com>
2026-07-09 23:17:07 +0530

    Initial project setup with React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <h1>🎓 CampusAssist AI</h1>
+
+      <h2>Student Support Chatbot</h2>
+
+      <p>
+        Welcome to CampusAssist AI.
+      </p>
+
+      <button>
+        Ask AI
+      </button>
+    </div>
+  );
+}
+
+export default App;
